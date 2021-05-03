257595e14fe90f7ee9a536cd22489e5f15766167
Shirley Berchel <berchel.shirley@gmail.com>
2021-05-03 17:27:23 +0200

    starting php poo lessons with test on class Animal

---
+++ class/animal.class.php
@@ -0,0 +1,67 @@
+<?php
+/**
+* * Initiation à la POO en php
+* * Class Animal
+*/
+
+class Animal {
+    //Attribut public
+        public  $name;
+        public  $type;
+    //Attribut privates
+        private $dob;
+        private $weight;
+        private $female;
+
+    //*Constructeurs
+    //est une fonction public
+    public function __construct(string $newName,string $newType,string $newDob,float $newWeight,bool $newFemale){
+        $this-> name = $newName;
+        $this-> type = $newType;
+        $this->setDob($newDob);
+        $this->setWeight($newWeight);
+        $this->setFemale($newFemale);
+    }
+
+    //Accesseurs et mutateurs
+        //**Getter
+        public function getDob(){
+            //this fait réference à element de la classe
+            return $this -> dob;
+        }
+
+        public function getWeight(){
+            return $this -> weight;
+        }
+
+        public function getFemale(){
+            return $this -> female;
+        }
+
+
+        //**Setter
+        public function setDob(string $newDob){
+            //teste si un parametre est une date
+            if((bool) strtotime($newDob)){
+                $this -> dob = $newDob;
+            }else{
+                //renvoie le nom de classe contacéner avec un msg
+                throw new Exception(__CLASS__.' : Le paramétre doit être une date');
+            }
+        }
+
+        public function setWeight(float $newWeight){
+            //test si le poids est cohérent 
+            if($newWeight < .2 || $newWeight > 1000){
+                throw new Exception(__CLASS__.' : Le poids dit être compris entre 200g et 1t.');
+            }else {
+                $this -> weight = $newWeight;
+            }
+        }
+
+        public function setFemale(bool $newFemale){
+            $this -> female = $newFemale;
+        }
+}
+
+?>
---
+++ tests/test_animal.php
@@ -0,0 +1,27 @@
+<?php 
+include_once('../class/animal.class.php');
+echo '<h2> Instanciation de la classe </h2>';
+
+/*$milou = new Animal();
+$milou -> name = 'Milou';
+//$milou -> dob = '1925-05-03';
+//$milou -> weight = 6.54;
+//$milou -> female = false;
+
+var_dump($milou);
+
+echo '<h2> Encapsulation getters et setters </h2>';
+
+//$milou -> setDob('toto');
+$milou -> setDob('2021-05-03');
+//$milou -> setWeight(.1);
+$milou -> setWeight(6.54);
+$milou -> setFemale('non');
+
+var_dump($milou);*/
+
+echo '<h2> Constructeurs </h2>';
+    $pet2 = new Animal('Garfield','chat','2021-05-03',7.8, false);
+    var_dump($pet2);
+
+?>
