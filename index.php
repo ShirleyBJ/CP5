888782b3f517e565456f85b8a2eb65cb6d4511d0
Shirley Berchel <berchel.shirley@gmail.com>
2021-05-03 12:30:03 +0200

    adding js file to check passwd

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" 
     integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" 
     crossorigin="anonymous">
+    <script src="js/script.js" defer></script>
     <title>Document</title>
 </head>
 
@@ -135,7 +136,7 @@
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
-                    <input type="submit" value="S'inscrire" class="btn btn-primary">
+                    <input type="submit" value="S'inscrire" class="btn btn-primary" id="btnValidate">
                 </div>
             </form>
         </div>
---
+++ js/script.js
@@ -0,0 +1,13 @@
+//récupération élements
+var btnValidate = document.querySelector('#register form');
+
+//Ajout écouteurs
+btnValidate.addEventListener('submit',function(){
+    alert("Test");
+});
+
+
+
+
+
+
